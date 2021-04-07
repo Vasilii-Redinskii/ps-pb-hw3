85f1ae05e4bce3799e66ce457b9c4ba56c1c2d94
Vasilii Redinskii <redinskiy@yandex.ru>
2021-04-07 08:57:48 +0300

    Vers2

--- Operator_if.py
+++ Operator_if.py
@@ -1,3 +1,4 @@
+# Make list & dictionaries
 cities = ['Москва', 'Париж', 'Лондон']
 
 users = [{'name': 'Иван', 'age': 35},
@@ -7,15 +8,18 @@ users = [{'name': 'Иван', 'age': 35},
 tourists = [{'user': users[0], 'city': cities[2]},
             {'user': users[1], 'city': cities[0]},
             {'user': users[2], 'city': cities[1]}]
-
+# Input city
 user_city = input('Введите город: ')
+# Make var city with capital letter
 city = user_city.capitalize()
-
+# Compare city with list
 if city in cities:
+    # If city in list display info about tourist
     i = 0
     for i in range(len(tourists)):
         tourist = tourists[i]
         if city == tourist['city']:
             print('Турист {} возраст {}. Посетил город {}.'.format(tourist['user']['name'],tourist['user']['age'],user_city))
+# if city out of the list print about it.
 else:
     print("Туристы не посещали этот город.")
